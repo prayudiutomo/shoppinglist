843f378b917a551be58b8e4fc00fafc48c5ab000
Yudi Utomo <yudi@codeintech.com>
2016-12-17 13:35:30 +0800

    First commit

---
+++ my.js
@@ -0,0 +1,11 @@
+class ShoppingList extends React.Component {
+ render(){
+   return ( <div>Hello From React </div> );
+ }
+}
+
+let root =  document.getElementById('root');
+
+ReactDOM.render(
+ <ShoppingList />, root
+)
