69d4129c7bd36af8e359dcb3e0121169e91fdaf1
Prayudi Utomo <yudi@codeintech.com>
2016-12-23 21:56:33 +0800

    Add Redux

--- src/index.js
+++ src/index.js
@@ -3,140 +3,98 @@ import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import { connect } from 'react-redux'
+import styles from '../assets/css/custom.css'
+
+let nextTodoId = 0
 
 // Actions
 export const increaseNumberBy1 = (number) => ({
-  type: 'INCREASE_NUMBER_BY_1',
-  number: number,
+	type: 'INCREASE_NUMBER_BY_1',
+	number: number,
 });
 
-// Reducers
-const numberReducer = (state = {number: 0}, action) => {
-  switch (action.type) {
-    case 'INCREASE_NUMBER_BY_1':
-      return {number: action.number + 1};
-    default:
-      return state
-  }
+export const addProduct = (title) => ({
+	type: 'ADD_PRODUCT',
+	id: nextTodoId++,
+	product: title,
+});
+
+export const editProduct = (id, product) => ({
+	type: 'EDIT_PRODUCT',
+	id: id,
+	product: product
+});
+
+export const setHighlight = (id) => ({
+	type: 'SET_HIGHLIGHT',
+	id: id,
+});
+
+export const setEditable = (id) => ({
+	type: 'SET_EDITABLE',
+	id: id,
+});
+
+export const deleteProduct = (id) => ({
+	type: 'DELETE_PRODUCT',
+	id: id,
+});
+
+const initialState = {
+	data:[{
+		id: Math.floor(Math.random()*90000) + 10000,
+		product: 'Apple Macbook',
+		highlight: false,
+		editable: false
+	}]
 }
 
-// number
+// Reducers
+const numberReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case 'ADD_PRODUCT':
+			const newProduct = {
+				id: action.id,
+				product: action.product,
+				highlight: false,
+				editable: false
+			};
+
+			return { data: [...state.data, newProduct] }
+
+		case 'EDIT_PRODUCT':
+			return { data: state.data.map(item => item.id === action.id ? 
+				Object.assign({}, item, {product: action.product, editable: !item.editable}) : item) }
+		case 'SAVE_PRODUCT':
+			return {...state, id: action.id};
+
+		case 'SET_HIGHLIGHT':
+			return { data: state.data.map(item => item.id === action.id ? 
+				Object.assign({}, item, {highlight: !item.highlight}) : item) }
+
+		case 'SET_EDITABLE':
+			return { data: state.data.map(item => item.id === action.id ? 
+				Object.assign({}, item, {editable: !item.editable}) : item) }
+
+		case 'DELETE_PRODUCT':
+			return { data: state.data.filter((v) => v.id !== action.id)}
+		default:
+			return state
+	}
+}
+
 let store = createStore(numberReducer);
 
 class ShoppingList extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			data: [
-				{
-					"id":"01",
-					"product":"iPhone 7",
-					"highlight":false,
-					"editable":false
-
-				},
-				{
-					"id":"02",
-					"product":"Macbookddd Pro",
-					"highlight":false,
-					"editable":false
-				},
-				{
-					"id":"03",
-					"product":"iPad",
-					"highlight":false,
-					"editable":false
-				}
-			]
-		};
-	}
-
-	generateId() {
-		return Math.floor(Math.random()*90000) + 10000;
-	}
-
-	handleItemUpdated(nodeId, product) {
-		var data = this.state.data;
-		var result = data.filter(function(v) {
-			return v.id === nodeId;
-		});
-
-		result[0].product = product;
-		result[0].editable = false;
-
-		this.setState({data})
-	}
-
-	handleItemRemoval(nodeId) {
-		var data = this.state.data;
-		data = data.filter(function (el) {
-			return el.id !== nodeId;
-		});
-
-		this.setState({data});
-
-		return;
-	}
-
-	handleItemAdd(product) {
-		var newItem = this.state.data;
-		newItem.push({
-			id: this.generateId().toString(),
-			product: product,
-			highlight: false,
-			editable: false
-		});
-
-		this.setState({newItem})
-	}
-
-	handleHightlight(nodeId) {
-		var data = this.state.data;
-		var result = data.filter(function(v) {
-			return v.id === nodeId;
-		});
-
-		if (result[0].highlight == false) {
-	   		result[0].highlight = true;
-		} else {
-	   		result[0].highlight = false;
-		}
-
-		this.setState({data})
-	}
-
-	handleEditable(nodeId) {
-		var data = this.state.data;
-		var result = data.filter(function(v) {
-			return v.id === nodeId;
-		});
-
-		if (result[0].editable == false) {
-	   		result[0].editable = true;
-		} else {
-	   		result[0].editable = false;
-		}
-
-		this.setState({data})
 	}
 
 	render() {
 		return (
 			<div className="container">
-				<div>{this.props.number}</div>
-				<button onClick={() => {
-						this.props.increaseNumberBy1(this.props.number);
-					}}>Increase</button>
 				<div className="col-sm-6">
-					<ProductList
-						data={this.state.data}
-						editable={this.state.editable}
-						handleEditable={this.handleEditable.bind(this)}
-						removeItem={this.handleItemRemoval.bind(this)}
-						updateItem={this.handleItemUpdated.bind(this)}
-						updateHightlight={this.handleHightlight.bind(this)}
-						addItem={this.handleItemAdd.bind(this)}
-					/>
+					<ProductList data={this.props.data}/>
 				</div>
 			</div>
 		);
@@ -144,16 +102,21 @@ class ShoppingList extends React.Component {
 }
 
 const mapStateToProps = (state) => ({
-  number: state.number,
+	data: state.data
 })
 
 const mapDispatchToProps =  ({
-  increaseNumberBy1: increaseNumberBy1,
+	increaseNumberBy1: increaseNumberBy1,
+	setEditable: setEditable,
+	setHighlight: setHighlight,
+	addProduct: addProduct,
+	editProduct: editProduct,
+	deleteProduct: deleteProduct
 })
 
 const VisibleShoppingList = connect(
-  mapStateToProps,
-  mapDispatchToProps
+	mapStateToProps,
+	mapDispatchToProps
 )(ShoppingList)
 
 class ProductList extends React.Component {
@@ -166,14 +129,10 @@ class ProductList extends React.Component {
 			return (
 				<VisibleProductItem
 					key={listItem.id}
-					nodeId={listItem.id}
+					id={listItem.id}
 					product={listItem.product}
-					highlight={listItem.highlight}
 					editable={listItem.editable}
-					handleEditable={this.props.handleEditable.bind(this)}
-					removeItem={this.props.removeItem.bind(this)}
-					updateItem={this.props.updateItem.bind(this)}
-					updateHightlight={this.props.updateHightlight.bind(this)}
+					highlight={listItem.highlight}
 				/>
 			);
 		},this);
@@ -197,7 +156,7 @@ class ProductList extends React.Component {
 					<h2>Shopping List</h2>
 				</li>
 				<li className="row captions">
-				<ProductForm addItem={this.props.addItem.bind(this)} />
+					<VisibleProductForm />
 				</li>
 				{listNodes}
 				<li className="row totals">
@@ -205,37 +164,17 @@ class ProductList extends React.Component {
 					<span className="order highlight">Highlighted: {numHighlight}</span>
 				</li>
 				<li className="highlightDetails">
-					<span className="order">Highlighted Item: {highlightNodes.filter(function(n){ return n != undefined }).join(', ')}</span>
+					<span className="order">Highlighted Item: {highlightNodes.filter(function(n){ return n !== undefined }).join(', ')}</span>
 				</li>
 			</ul>
 		);
 	}
 }
 
+
 class ProductItem extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = { text: '' };
-	}
-
-	updateItem(e) {
-		this.props.updateItem(this.props.nodeId, this.refs.edit.value);
-	}
-
-	removeItem(e) {
-		e.preventDefault();
-		this.props.removeItem(this.props.nodeId);
-		return;
-	}
-
-	handleEditable(e) {
-		e.preventDefault();
-		this.props.handleEditable(this.props.nodeId);
-		return;
-	}
-
-	updateHightlight(e) {
-		this.props.updateHightlight(this.props.nodeId);
 	}
 
 	render() {
@@ -247,30 +186,29 @@ class ProductItem extends React.Component {
 		}
 
 		return (
-			<li
-				className={className} >
-				  <span>{this.props.number} </span>
-					<span className="itemName">
-						{this.props.editable ?
-							<input type="text" defaultValue={this.props.product} ref="edit"/>
-		        :
-							<label onClick={this.updateHightlight.bind(this)}>
-								{this.props.product}
-							</label>
-		        }
-					</span>
-					<span className="action" onClick={(this.props.editable) ? this.updateItem.bind(this) : this.handleEditable.bind(this)}>
-            {(this.props.editable) ? "Save" : "Edit"}
-          </span>
-					<span className="action" onClick={this.removeItem.bind(this)}>Delete</span>
+			<li className={className} >
+				<span>{this.props.number} </span>
+				<span className="itemName"> 
+					{ this.props.editable ? 
+						<input type="text" defaultValue={this.props.product} ref="edit"/>
+						:
+						<label onClick={() => { this.props.setHighlight(this.props.id); }}>
+							{this.props.product}
+						</label>
+					}
+				</span>
+				<span className="action" onClick={() => { (this.props.editable)? this.props.editProduct(this.props.id, this.refs.edit.value):this.props.setEditable(this.props.id); }}>
+					{(this.props.editable) ? "Save" : "Edit"}
+				</span>
+				<span className="action" onClick={() => { this.props.deleteProduct(this.props.id); }}>Delete</span>
 			</li>
-			);
+		);
 	}
 }
 
 const VisibleProductItem = connect(
-  mapStateToProps,
-  mapDispatchToProps
+	mapStateToProps,
+	mapDispatchToProps
 )(ProductItem)
 
 
@@ -281,7 +219,7 @@ class ProductForm extends React.Component {
 
 	doSubmit(e) {
 		e.preventDefault();
-		this.props.addItem(this.refs.product.value);
+		this.props.addProduct(this.refs.product.value);
 		this.refs.product.value = '';
 		return;
 	}
@@ -289,18 +227,26 @@ class ProductForm extends React.Component {
 	render() {
 		return (
 			<form onSubmit={this.doSubmit.bind(this)}>
-				<span><input type="text" id="product" ref="product" className="form-control" placeholder="Add product" /></span>
-				<span><input type="submit" value="Add" className="btn" /></span>
+			<span><input type="text" id="product" ref="product" className="form-control" placeholder="Add product" /></span>
+			<span><input type="submit" value="Add" className="btn" /></span>
 			</form>
-		);
+			);
 	}
 }
+
+
+
+const VisibleProductForm = connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(ProductForm)
+
 
 let root =  document.getElementById('root');
 
 ReactDOM.render(
 	<Provider store={store}>
-		<VisibleShoppingList />
+	<VisibleShoppingList />
 	</Provider>,
 	root
 	)
