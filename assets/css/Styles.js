4ab1bd2d3007f9b8b2a033feba60dedbc976c651
Prayudi Utomo <yudi@codeintech.com>
2016-12-28 16:20:42 +0800

    Change css to js

--- assets/css/Styles.js
+++ assets/css/Styles.js
@@ -6,128 +6,82 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-	* {
-		box-sizing: border-box,
-	},
-	body{
+	body: {
 		fontSize : 16,
 		backgroundColor: '#f2f6f9',
-		fontFamily: Helvetica, Arial, sans-serif,
+		fontFamily: 'Helvetica',
 	},
-	h2 {
-		textTransform: capitalize,
-	},
-	ul{
-		listStyleType: none,
-	},
-	li {
-	    display: list-item,
-	    textAlign: -webkit-match-parent,
-	},
-	a:hover {
-		textDecoration:none,
-	},
-	.highlight .itemName {
-		fontWeight: bold,
-	},
-	.highlightDetails {
+	highlightDetails: {
 			marginTop: 0,
-			clear: both,
-			display: block,
 	},
-	.container {
-	    marginRight: auto,
-	    marginLeft: auto,
+	container: {
 	    paddingLeft: 15,
 	    paddingRight: 15,
 	},
-	.col-sm-6 {
-	    width: 50%,
+	colSm6: {
+	    width: 50,
 	},
-	.row {
+	row: {
 	    paddingBottom: 0,
 	    paddingLeft: 15,
 	    backgroundColor: '#ffffff',
 	    marginBottom: 11,
-	},
-	.row {
 	    marginLeft: -15,
 	    marginRight: -15,
 	},
-	.row:before, .row:after {
-		clear: both,
-		content: " ",
-		display: table,
+	span: {
+		padding: 10,
 	},
-	.row span{
-		padding: 10 0 6 0,
-	},
-	.captions{
+	captions: {
 		color: '#7e93a7',
 		fontSize:12,
-		textTransform: uppercase,
 		padding: 0,
-		boxShadow: 0 0 0,
 		backgroundColor: '#f2f6f9',
 		marginTop: 20,
 	},
-	.captions span{
-		padding: 0 21 0 0,
+	span: {
+		paddingRight: 20,
 	},
-	.itemName{
+	itemName: {
 		color: '#727578',
 		fontSize :16,
-		float: left,
 		paddingLeft:25,
-		cursor: pointer,
 	},
-	.action{
+	action: {
 		color: '#f06953',
-		fontWeight: bold,
-		float: right,
 		marginRight: 10,
-		cursor: pointer,
 	},
-	.totals{
+	totals: {
 		backgroundColor: '#f2f6f9',
 	},
-	.totals span{
-		padding: 10 15 10 0,
+	span: {
+		padding: 10,
 	},
-	.totals .action{
-		float: left,
-	},
-	.totals .itemName{
+	itemName: {
 		marginTop: 1,
 	},
-	.totals .order{
-		float: right,
+	order: {
 		padding: 0,
 		marginTop: 10,
 		paddingLeft: 5,
 	},
-	.order {
+	order: {
 		color: '#f06953',
-		fontWeight: bold,
 	},
-	.form-control {
-		width: 67%,
-		display: inline-block,
+	formControl: {
+		width: 67,
 		fontSize: 14,
 		paddingLeft: 15,
 		paddingRight: 15,
 	},
-	.btn {
-			font-size: 16,
+	btn: {
+			fontSize: 16,
 	},
-	label {
-			padding: 8 10 5 10,
-			border: 1 solid #fff,
+	label: {
+			padding: 10,
+			borderColor: '#fff',
+			borderWidth: 1,
 	},
-	label[contenteditable='true'] {
-	    border: 1 solid #aaa,
-			backgroundColor: '#efefef',
-	}
 });
 
 module.exports = styles
