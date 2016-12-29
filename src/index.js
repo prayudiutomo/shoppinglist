d6aae1802464ce7aafc106cc38a3f8de0bf9b4b4
Prayudi Utomo <yudi@codeintech.com>
2016-12-29 16:31:00 +0800

    Add hardcode list

--- src/index.js
+++ src/index.js
@@ -10,43 +10,6 @@ import { Provider } from 'react-redux'
 import { connect } from 'react-redux'
 import Styles from '../assets/css/Styles';
 
-// Actions
-export const addProduct = (title) => ({
-	type: 'ADD_PRODUCT',
-	id: nextTodoId++,
-	product: title,
-});
-
-export const editProduct = (id, product) => ({
-	type: 'EDIT_PRODUCT',
-	id: id,
-	product: product
-});
-
-export const setHighlight = (id) => ({
-	type: 'SET_HIGHLIGHT',
-	id: id,
-});
-
-export const setEditable = (id) => ({
-	type: 'SET_EDITABLE',
-	id: id,
-});
-
-export const deleteProduct = (id) => ({
-	type: 'DELETE_PRODUCT',
-	id: id,
-});
-
-const initialState = {
-	data:[{
-		id: Math.floor(Math.random()*90000) + 10000,
-		product: 'Apple Macbook',
-		highlight: false,
-		editable: false
-	}]
-}
-
 class ShoppingList extends React.Component {
 	// Initialize the hardcoded data
 	constructor(props) {
@@ -54,7 +17,7 @@ class ShoppingList extends React.Component {
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
       dataSource: ds.cloneWithRows([
-        'John', 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
+        'Macbook Pro', 'iPad', 'iPhone', 'Samsung Galaxy', 'Lenovo Note', 'Xiaomi', 'Sony XPeria', 'Blackberry'
       ])
     };
   }
@@ -64,13 +27,11 @@ class ShoppingList extends React.Component {
       <View style={{flex: 1, paddingTop: 22}}>
         <ListView
           dataSource={this.state.dataSource}
-          renderRow={(rowData) => <Text>{rowData}</Text>}
+          renderRow={(rowData) => <Text>{rowData} Delete Edit</Text>}
         />
       </View>
     );
   }
 }
-
-//AppRegistry.registerComponent('shoppinglistrn', () => ShoppingList);
 
 export default ShoppingList;
